8795c7dbb64c8c8b465dbb9f08a62c64bf7cfec9
lkqstart <55475086+lkqstart@users.noreply.github.com>
2024-11-09 07:31:43 +0800

    Update keep_serv00_login.sh
    1

--- keep_serv00_login.sh
+++ keep_serv00_login.sh
@@ -75,13 +75,16 @@ for account in $accounts; do
     echo "正在连接 $username@$ip ..."
     if sshpass -p "$password" ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ConnectTimeout=60 -o ServerAliveInterval=30 -o ServerAliveCountMax=2 -tt "$username@$ip" "sleep 3; exit"; then
         echo "成功激活 $username@$ip"
-	send_telegram_message " 服务器登录成功,当前登录信息: 
- 				总共有 $total_accounts 个用户, 
-     				用户名 : $username,服务器: $ip 
-	 			登录时间: $(TZ="Asia/Shanghai" date "+%Y-%m-%d %H:%M:%S")"
+	send_telegram_message 
+ 	"服务器登录成功通知：
+	总共有 *$total_accounts* 个用户，
+	用户名: *$username*
+	服务器: *$ip*
+	登录时间: $(TZ="Asia/Shanghai" date "+%Y-%m-%d %H:%M:%S")请及时查看。"
+
     else
         echo "连接激活 $username@$ip 失败"
-        send_telegram_message "serv00激活失败,正在重新激活...: $username@$ip"
+        send_telegram_message "serv00激活失败,正在重试中...: $username@$ip"
     fi
     echo "----------------------------"
 done
